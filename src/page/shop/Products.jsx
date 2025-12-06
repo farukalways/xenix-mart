546d287f5a287f69321607aa68ec51032729a89b
farukalways <alishopon367@gmail.com>
2025-12-06 08:14:26 +0600

    feat: pagination add in products components

--- src/page/shop/Products.jsx
+++ src/page/shop/Products.jsx
@@ -5,12 +5,18 @@ import Product from "./Product";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const limit = 15;
 
   useEffect(() => {
     let ignore = false;
     const fetchProducts = async () => {
-      const response = await fetch("https://dummyjson.com/products");
+      const skip = (page - 1) * limit;
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=${limit}&skip=${skip}`
+      );
       const result = await response.json();
+      console.log(result);
       if (!ignore) return setProducts(result.products);
     };
 
@@ -19,14 +25,38 @@ const Products = () => {
     return () => {
       ignore = true;
     };
-  }, []);
+  }, [page]);
 
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-3 gap-5">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-    </div>
+    <section>
+      {products.length === 0 ? (
+        <h2>loading...</h2>
+      ) : (
+        <>
+          {" "}
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+            {products.map((product) => (
+              <Product key={product.id} product={product} />
+            ))}
+          </div>
+          <div className="pt-8 pb-3 flex items-center justify-center gap-5">
+            <button
+              onClick={() => setPage((prev) => prev - 1)}
+              className="px-4 py-2 text-[#000000] rounded"
+            >
+              Prev
+            </button>
+            <button className="px-4 py-2 text-[#000000] rounded">{page}</button>
+            <button
+              onClick={() => setPage((prev) => prev + 1)}
+              className="px-4 py-2 text-[#000000] rounded"
+            >
+              Next
+            </button>
+          </div>
+        </>
+      )}
+    </section>
   );
 };
 
--- src/page/shop/Shop.jsx
+++ src/page/shop/Shop.jsx
@@ -3,16 +3,19 @@ import Products from "./Products";
 const Shop = () => {
   return (
     <section className="flex min-h-screen">
-      <div className="w-1/5 bg-blue-400 p-4 sticky top-0 h-screen">
-        <h2>filter section</h2>
-        <h2>filter section</h2>
-        <h2>filter section</h2>
-        <h2>filter section</h2>
-        <h2>filter section</h2>
-      </div>
-      <div className="w-4/5 bg-[#FFFFFF] p-4 overflow-y-auto  ">
+      {/* Sidebar */}
+      <aside className="hidden md:block md:w-1/5 bg-blue-400 p-4 sticky top-0 h-screen overflow-y-auto">
+        <h2>Filter section</h2>
+        <h2>Filter section</h2>
+        <h2>Filter section</h2>
+        <h2>Filter section</h2>
+        <h2>Filter section</h2>
+      </aside>
+
+      {/* Main Content */}
+      <main className="w-full md:w-4/5 bg-white p-4 overflow-y-auto">
         <Products />
-      </div>
+      </main>
     </section>
   );
 };
